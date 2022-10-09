5393e9a846487af5c727744bc50ad413811c3cb4
lorenvtvss <58482295+lorenvtvss@users.noreply.github.com>
2022-10-09 21:26:19 +0200

    Update asymstab.m

--- asymstab.m
+++ asymstab.m
@@ -10,9 +10,9 @@ bordertol=1e-8; lo=bordertol; hi=1-bordertol; tol=1e-7;
 xl=length(xvec); F=zeros(xl,1); f=F;
 
 for loop=1:length(xvec)
-    x=xvec(loop); f(loop)=quadl(@fff, lo, hi, tol, [], x, a, b, 1) / pi;
+    x=xvec(loop); f(loop)=quadl(@fff, lo, hi, tol, [], x, a, b, 1) / pi;    %% change quadl!
     if nargout>1
-        F(loop)=0.5-(1/pi)*quadl(@fff, lo, hi, tol, [], x, a, b, 0);
+        F(loop)=0.5-(1/pi)*quadl(@fff, lo, hi, tol, [], x, a, b, 0);        %% change quadl
     end
 end
 
